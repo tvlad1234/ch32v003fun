58f694470dd292bb43b3110efc0cc0dae4a5fb50
tvlad1234 <vlad.tomoiaga@gmail.com>
2023-12-22 14:27:20 +0200

    rename all mini_ functions

--- ch32v003fun/ch32v003fun.c
+++ ch32v003fun/ch32v003fun.c
@@ -14,8 +14,8 @@
 
 int errno;
 
-int mini_vsnprintf(char *buffer, unsigned int buffer_len, const char *fmt, va_list va);
-int mini_vpprintf(int (*puts)(char* s, int len, void* buf), void* buf, const char *fmt, va_list va);
+int vsnprintf(char *buffer, unsigned int buffer_len, const char *fmt, va_list va);
+int vpprintf(int (*puts)(char* s, int len, void* buf), void* buf, const char *fmt, va_list va);
 
 static int __puts_uart(char *s, int len, void *buf)
 {
@@ -27,7 +27,7 @@ int printf(const char* format, ...)
 {
 	va_list args;
 	va_start( args, format );
-	int ret_status = mini_vpprintf(__puts_uart, 0, format, args);
+	int ret_status = vpprintf(__puts_uart, 0, format, args);
 	va_end( args );
 	return ret_status;
 }
@@ -382,21 +382,21 @@ int puts(const char *s)
  *
  * This is a minimal snprintf() implementation optimised
  * for embedded systems with a very limited program memory.
- * mini_snprintf() doesn't support _all_ the formatting
+ * snprintf() doesn't support _all_ the formatting
  * the glibc does but on the other hand is a lot smaller.
  * Here are some numbers from my STM32 project (.bin file size):
  *      no snprintf():      10768 bytes
  *      mini snprintf():    11420 bytes     (+  652 bytes)
  *      glibc snprintf():   34860 bytes     (+24092 bytes)
  * Wasting nearly 24kB of memory just for snprintf() on
- * a chip with 32kB flash is crazy. Use mini_snprintf() instead.
+ * a chip with 32kB flash is crazy. Use snprintf() instead.
  *
  */
 
-#define mini_strlen strlen
+#define strlen strlen
 
 static int
-mini_itoa(long value, unsigned int radix, int uppercase, int unsig,
+itoa(long value, unsigned int radix, int uppercase, int unsig,
 	 char *buffer)
 {
 	char	*pbuffer = buffer;
@@ -437,7 +437,7 @@ mini_itoa(long value, unsigned int radix, int uppercase, int unsig,
 }
 
 static int
-mini_pad(char* ptr, int len, char pad_char, int pad_to, char *buffer)
+pad(char* ptr, int len, char pad_char, int pad_to, char *buffer)
 {
 	int i;
 	int overflow = 0;
@@ -462,7 +462,7 @@ mini_pad(char* ptr, int len, char pad_char, int pad_to, char *buffer)
 	return len;
 }
 
-struct mini_buff {
+struct buff {
 	char *buffer, *pbuffer;
 	unsigned int buffer_len;
 };
@@ -471,7 +471,7 @@ static int
 _puts(char *s, int len, void *buf)
 {
 	if(!buf) return len;
-	struct mini_buff *b = buf;
+	struct buff *b = buf;
 	char * p0 = b->buffer;
 	int i;
 	/* Copy to buffer */
@@ -485,31 +485,31 @@ _puts(char *s, int len, void *buf)
 	return b->pbuffer - p0;
 }
 
-#ifdef MINI_PRINTF_ENABLE_OBJECTS
-static int (*mini_handler) (void* data, void* obj, int ch, int lhint, char** bf) = 0;
-static void (*mini_handler_freeor)(void* data, void*) = 0;
-static void * mini_handler_data = 0;
+#ifdef PRINTF_ENABLE_OBJECTS
+static int (*handler) (void* data, void* obj, int ch, int lhint, char** bf) = 0;
+static void (*handler_freeor)(void* data, void*) = 0;
+static void * handler_data = 0;
 
-void mini_printf_set_handler(
+void printf_set_handler(
 	void* data,
 	int (*handler)(void* data, void* obj, int ch, int len_hint, char** buf),
 	void (*freeor)(void* data, void* buf))
 {
-	mini_handler = handler;
-	mini_handler_freeor = freeor;
-	mini_handler_data = data;
+	handler = handler;
+	handler_freeor = freeor;
+	handler_data = data;
 }
 #endif
 
 int
-mini_vsnprintf(char *buffer, unsigned int buffer_len, const char *fmt, va_list va)
+vsnprintf(char *buffer, unsigned int buffer_len, const char *fmt, va_list va)
 {
-	struct mini_buff b;
+	struct buff b;
 	b.buffer = buffer;
 	b.pbuffer = buffer;
 	b.buffer_len = buffer_len;
 	if(buffer_len == 0) buffer = (void*) 0;
-	int n = mini_vpprintf(_puts, (buffer != (void*)0)?&b:(void*)0, fmt, va);
+	int n = vpprintf(_puts, (buffer != (void*)0)?&b:(void*)0, fmt, va);
 	if(buffer == (void*) 0) {
 		return n;
 	}
@@ -517,12 +517,12 @@ mini_vsnprintf(char *buffer, unsigned int buffer_len, const char *fmt, va_list v
 }
 
 int
-mini_vpprintf(int (*puts)(char* s, int len, void* buf), void* buf, const char *fmt, va_list va)
+vpprintf(int (*puts)(char* s, int len, void* buf), void* buf, const char *fmt, va_list va)
 {
 	char bf[24];
 	char bf2[24];
 	char ch;
-#ifdef MINI_PRINTF_ENABLE_OBJECTS
+#ifdef PRINTF_ENABLE_OBJECTS
 	void* obj;
 #endif
 	if(puts == (void*)0) {
@@ -563,57 +563,57 @@ mini_vpprintf(int (*puts)(char* s, int len, void* buf), void* buf, const char *f
 				case 'u':
 				case 'd':
 					if(l) {
-						len = mini_itoa(va_arg(va, unsigned long), 10, 0, (ch=='u'), bf2);
+						len = itoa(va_arg(va, unsigned long), 10, 0, (ch=='u'), bf2);
 					} else {
 						if(ch == 'u') {
-							len = mini_itoa((unsigned long) va_arg(va, unsigned int), 10, 0, 1, bf2);
+							len = itoa((unsigned long) va_arg(va, unsigned int), 10, 0, 1, bf2);
 						} else {
-							len = mini_itoa((long) va_arg(va, int), 10, 0, 0, bf2);
+							len = itoa((long) va_arg(va, int), 10, 0, 0, bf2);
 						}
 					}
-					len = mini_pad(bf2, len, pad_char, pad_to, bf);
+					len = pad(bf2, len, pad_char, pad_to, bf);
 					len = puts(bf, len, buf);
 					break;
 
 				case 'x':
 				case 'X':
 					if(l) {
-						len = mini_itoa(va_arg(va, unsigned long), 16, (ch=='X'), 1, bf2);
+						len = itoa(va_arg(va, unsigned long), 16, (ch=='X'), 1, bf2);
 					} else {
-						len = mini_itoa((unsigned long) va_arg(va, unsigned int), 16, (ch=='X'), 1, bf2);
+						len = itoa((unsigned long) va_arg(va, unsigned int), 16, (ch=='X'), 1, bf2);
 					}
-					len = mini_pad(bf2, len, pad_char, pad_to, bf);
+					len = pad(bf2, len, pad_char, pad_to, bf);
 					len = puts(bf, len, buf);
 					break;
 
 				case 'c' :
 					ch = (char)(va_arg(va, int));
-					len = mini_pad(&ch, 1, pad_char, pad_to, bf);
+					len = pad(&ch, 1, pad_char, pad_to, bf);
 					len = puts(bf, len, buf);
 					break;
 
 				case 's' :
 					ptr = va_arg(va, char*);
-					len = mini_strlen(ptr);
+					len = strlen(ptr);
 					if (pad_to > 0) {
-						len = mini_pad(ptr, len, pad_char, pad_to, bf);
+						len = pad(ptr, len, pad_char, pad_to, bf);
 						len = puts(bf, len, buf);
 					} else {
 						len = puts(ptr, len, buf);
 					}
 					break;
-#ifdef MINI_PRINTF_ENABLE_OBJECTS
+#ifdef PRINTF_ENABLE_OBJECTS
 				case 'O' :  /* Object by content (e.g. str) */
 				case 'R' :  /* Object by representation (e.g. repr)*/
 					obj = va_arg(va, void*);
-					len = mini_handler(mini_handler_data, obj, ch, pad_to, &ptr);
+					len = handler(handler_data, obj, ch, pad_to, &ptr);
 					if (pad_to > 0) {
-						len = mini_pad(ptr, len, pad_char, pad_to, bf);
+						len = pad(ptr, len, pad_char, pad_to, bf);
 						len = puts(bf, len, buf);
 					} else {
 						len = puts(ptr, len, buf);
 					}
-					mini_handler_freeor(mini_handler_data, ptr);
+					handler_freeor(handler_data, ptr);
 					break;
 #endif
 				default:
@@ -630,24 +630,24 @@ end:
 
 
 int
-mini_snprintf(char* buffer, unsigned int buffer_len, const char *fmt, ...)
+snprintf(char* buffer, unsigned int buffer_len, const char *fmt, ...)
 {
 	int ret;
 	va_list va;
 	va_start(va, fmt);
-	ret = mini_vsnprintf(buffer, buffer_len, fmt, va);
+	ret = vsnprintf(buffer, buffer_len, fmt, va);
 	va_end(va);
 
 	return ret;
 }
 
 int
-mini_pprintf(int (*puts)(char*s, int len, void* buf), void* buf, const char *fmt, ...)
+pprintf(int (*puts)(char*s, int len, void* buf), void* buf, const char *fmt, ...)
 {
 	int ret;
 	va_list va;
 	va_start(va, fmt);
-	ret = mini_vpprintf(puts, buf, fmt, va);
+	ret = vpprintf(puts, buf, fmt, va);
 	va_end(va);
 
 	return ret;
